7c6648c4c3eeec9f648e5df192aa8eb402e8c1ee
Jeffrey Han <itdelatrisu@gmail.com>
2015-01-12 20:51:53 -0500

    Changed command-line parameters to system properties.
    Signed-off-by: Jeffrey Han <itdelatrisu@gmail.com>

--- src/org/ninjacave/jarsplice/gui/IntroductionPanel.java
+++ src/org/ninjacave/jarsplice/gui/IntroductionPanel.java
@@ -22,7 +22,7 @@ public class IntroductionPanel extends JPanel
       String.format("<html><div style=\"width:%dpx;\">%s</div><html>", new Object[] { 
       Integer.valueOf(300), 
       "JarSplicePlus is an extension to JarSplice.<br/>"+
-      "In order to see the command-line options, run:<pre>$ java -jar JarSplicePlus.jar -h</pre><br/>"+
+      "In order to see the command-line options, run:<pre>$ java -jar -Dhelp JarSplicePlus.jar</pre><br/>"+
       "JarSplice is a fat executable jar creator. It aims to make deployment of Java applications with multiple jars and/or natives easy by creating just a single executable jar from them."+
       "<br/><br/>Its easy to use, just complete the following four steps:<br/><br/>1) Add Jars<br/><br/>2) Add Natives<br/><br/>3) Add Main Class<br/><br/>4) Create Fat Jar" }));
 
